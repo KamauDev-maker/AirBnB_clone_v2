3f628f3c91ebf2f2e42bd239759f39471e082339
KamauDev-maker <oscarnjenga@gmail.com>
2023-04-05 18:40:44 +0000

    initial setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,16 +2,16 @@
 # That sets up your web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/static/releases/test/
 echo '<html>
 <head>
 </head>
 <body>
-Best School
+Holberton School
 </body>
 </html>' | sudo tee /data/web_static/releases/test/index.html
-ln -s /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i "/listen 80 default_server;/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
-service nginx restart
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i "/listen 80 default_server;/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
+sudo service nginx restart
