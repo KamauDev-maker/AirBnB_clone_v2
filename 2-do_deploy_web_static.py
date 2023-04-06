282e0e43677c6d1702db2498538b6821f68a1848
KamauDev-maker <oscarnjenga@gmail.com>
2023-04-06 12:33:32 +0300

    corrections on deploying archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,32 +15,32 @@ def do_deploy(archive_path):
     """
     a method that distributes an archive to your web servers and deploy it
     """
-    if not exists(archive_path):
+    if not os.path.exists(archive_path):
         return False
+    filename = os.path.basename(archive_path)
+    filename_without_ext = filename.replace(".tgz", "")
+    filepath = "/data/web_static/releases/{}/".format(filename_without_ext)
+    success = False
     try:
         # upload the archive to the /tmp/
-        put(archive_path, '/tmp/')
-
-        # filename without extension
-        filename = archive_path.split('/')[-1]
-        filename_without_ext = filename.split('.')[0]
+        put(archive_path, '/tmp/{}'.format(filename)
 
         # Uncompress the archive to /data/web_static/releases/<archive
-        run("mkdir -p /data/web_static/releases/{}"
-            .format(filename_without_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(filename, filename_without_ext))
+        run("mkdir -p {}".format(filepath)
+        run("tar -xzf /tmp/{} -C {}".format(filename, filepath))
 
-        # Delete the archive from the web server
-        run("rm /tmp/{}".format(filename))
+       # Delete the archive from the web server
+        run("rm -rf /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(filepath, filepath))
+        run("rm -rf {}web_static".format(filepath))
 
         # Delete the symbolic link
         run("sudo rm -f /data/web_static/current")
 
         # Create a new the symbolic link
-        run("sudo ln -s /data/web_static/releases/{}/ "
-            "/data/web_static/current".format(filename_without_ext))
-
-        return True
+        run("sudo ln -s {} /data/web_static/current".format(filepath))
+        print("New version deployed!")
+        success = True
     except BaseException:
-        return False
+        success = False
+    return success
